caeea95bca0e43d9bdeda22b2ff82f05f575188d
johnny4fun <309329794@qq.com>
2020-03-11 00:07:14 +0800

    抛弃nord,显示信息太少了

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,8 @@
 # Base16 Shell
-if status --is-interactive
-    set BASE16_SHELL "$HOME/.config/base16-shell/"
-    source "$BASE16_SHELL/profile_helper.fish"
-end
+#if status --is-interactive
+#    set BASE16_SHELL "$HOME/.config/base16-shell/"
+#    source "$BASE16_SHELL/profile_helper.fish"
+#end
 
 if test "$TMUX" 
     tmux attach -t (whoami)||tmux new -s (whoami)
